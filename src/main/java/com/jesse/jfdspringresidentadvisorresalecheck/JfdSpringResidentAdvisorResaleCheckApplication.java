97f50a9d8785ac786bc7010083e40d95cdd28d1f
Jesse <jesse_dasilva@live.com>
2021-06-01 19:02:47 +0100

    feauture-User_Input:
    1. Create main method to take in user input parameters

--- src/main/java/com/jesse/jfdspringresidentadvisorresalecheck/JfdSpringResidentAdvisorResaleCheckApplication.java
+++ src/main/java/com/jesse/jfdspringresidentadvisorresalecheck/JfdSpringResidentAdvisorResaleCheckApplication.java
@@ -1,15 +1,21 @@
 package com.jesse.jfdspringresidentadvisorresalecheck;
 
-import com.jesse.jfdspringresidentadvisorresalecheck.services.ResidentAdvisorResaleService;
-import com.jesse.jfdspringresidentadvisorresalecheck.services.ResidentAdvisorResaleServiceImpl;
+import com.jesse.jfdspringresidentadvisorresalecheck.controller.MasterController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JfdSpringResidentAdvisorResaleCheckApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		SpringApplication.run(JfdSpringResidentAdvisorResaleCheckApplication.class, args);
+
+		String url = args[0];
+		String refreshPeriod = args[1];
+		String ticketName = args[2];
+
+		MasterController masterController = new MasterController();
+		masterController.Controller(url, refreshPeriod, ticketName);
 
 	}
 
